a64c5e768bea6d93e7507cbb10c8b40baee5f3de
Patrick Linode <58037348+PatrickStaehli@users.noreply.github.com>
2020-11-05 08:44:32 +0100

    scrap_data.py - Observes now if the get request is successful
    The status code of the request is checkt: if it is 200, the the request is successful. If not, the request failed and consequntly, the database is not updated.
    The status code for each centre id is also printed for each request to the output file.

--- scraper/scrap_data.py
+++ scraper/scrap_data.py
@@ -38,21 +38,28 @@ def request_occupancy(centres):
     ts = time.gmtime()
     timestamp = time.strftime("%Y-%m-%d %H:%M:%S", ts)
     conn = create_connection(r"../database/centre_occupation.db")
+    centre_status = {} #Get response status for each centre. 200 is success
     for centre in centres:
         url = 'https://blfa-api.migros.ch/fp/api/center/' + str(centre) + '/currentuser/'
-        response = requests.get(url, verify=False).json()
-        
-        current_visit = response['currentVisitors']
-        
-        if type(current_visit) == int:
-            add_occupancy(conn,(centre, current_visit, response['maxVisitors'], timestamp))
-        else:
-            current_visit = 0
-            add_occupancy(conn,(centre, current_visit, response['maxVisitors'], timestamp))
+        response = requests.get(url, verify=False)
 
+        if response.status_code == 200: #If the request is successful
+            centre_status[centre] = response.status_code # save the status code
+            response = response.json() #jsonify the request
+
+            current_visit = response['currentVisitors']
+        
+            if type(current_visit) == int:
+                add_occupancy(conn,(centre, current_visit, response['maxVisitors'], timestamp))
+            else:
+                current_visit = 0
+                add_occupancy(conn,(centre, current_visit, response['maxVisitors'], timestamp))
+        else:
+            centre_status[centre] = response.status_code # save the status code
+            
     
     conn.close()
-    return 
+    return centre_status
 # --------------------------------------------------------------------------------------
 
 
@@ -64,10 +71,15 @@ def request_all_centers():
     conn.close()
 
     centre_ids = [row[0] for row in rows]
-    print("Requesting centre occupancy")
-    request_occupancy(centre_ids)
+    request_center_status = request_occupancy(centre_ids)
     
-    print("Waiting 15 Minutes")
+    ts = time.gmtime()
+    timestamp = time.strftime("%Y-%m-%d %H:%M:%S", ts)
+    
+    print(timestamp)
+    print("Request status:")
+    print(request_center_status)
+    print("----------------------------------------------------------------------------------------------------------------------------------------------\n \n")
     time.sleep(60*15)
 
     
