0c448a3766621f4abc4a6b86fad6904c14bd328c
Patrick <58037348+PatrickStaehli@users.noreply.github.com>
2021-05-04 09:48:09 +0100

    Cleaned up some spelling errors

--- scraper/scrap_data.py
+++ scraper/scrap_data.py
@@ -1,30 +1,17 @@
-import sqlite3
-from sqlite3 import Error
 import requests
 from db_lib import SQLiteConnector
 requests.packages.urllib3.disable_warnings() #Disable ssl warnings
 import time
 
-# -------------------------------------------------------------------------------------
-def create_connection(db_file):
-    """ create a database connection to the SQLite database
-        specified by db_file
-    :param db_file: database file
-    :return: Connection object or None
-    """
-    
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-        return conn
-    except Error as e:
-        print(e)
-    
-    return conn
 
-def add_occupancy(conn, data):
-    ts = time.gmtime()
-    timestamp = time.strftime("%Y-%m-%d %H:%M:%S", ts)
+def add_occupancy_to_database(conn, data):
+    '''
+    Inserts a new row with occupancy data into the occupancy table
+    
+    :param  db_file: sqlite connector Connector to the database
+            data: tuple in form of (centre_id, current_visitors, current_max_visitors, timestamp)
+    '''
+    
     sql = ''' INSERT INTO occupancy(centre_id,currentVisitors,maxVisitors,timestamp)
               VALUES(?,?,?,?) '''
     cur = conn.cursor()
@@ -34,8 +21,17 @@ def add_occupancy(conn, data):
     return 
 
 
-# request the current occupancy
 def request_occupancy(centres):
+    '''
+    Requests the current centre occupation via get request from the url:
+    'https://blfa-api.migros.ch/fp/api/center/<center_id>/currentuser/' and 
+    inserts the data into the database.
+    
+    :param: centres: List of all centre id'save
+    
+    :return: List of request status for each centre. 200 means a successful request.
+    '''
+    
     ts = time.gmtime()
     timestamp = time.strftime("%Y-%m-%d %H:%M:%S", ts)
     cnxn = SQLiteConnector(r"../database/centre_occupation.db")
@@ -52,19 +48,24 @@ def request_occupancy(centres):
             current_visit = response['currentVisitors']
         
             if type(current_visit) == int:
-                add_occupancy(cnxn.connection,(centre, current_visit, response['maxVisitors'], timestamp))
+                add_occupancy_to_database(cnxn.connection,(centre, current_visit, response['maxVisitors'], timestamp))
             else:
                 current_visit = 0
-                add_occupancy(cnxn.connection,(centre, current_visit, response['maxVisitors'], timestamp))
+                add_occupancy_to_database(cnxn.connection,(centre, current_visit, response['maxVisitors'], timestamp))
         else:
             centre_status[centre] = response.status_code # save the status code
             
     cnxn.close_connection()
+    
     return centre_status
-# --------------------------------------------------------------------------------------
 
 
 def request_all_centers():
+    '''
+    Requests the centre occupancy for all centres that are stored in the
+    centre_occupation - centres table.
+    The request status code is written to the scraper_output.txt file.
+    '''
     cnxn = SQLiteConnector(r"../database/centre_occupation.db")
     
     cur = cnxn.connection.cursor()
@@ -85,12 +86,8 @@ def request_all_centers():
         f.write("\n----------------------------------------------------------------------------------------------------------------------------------------------\n \n")
         f.close()
 
-    #print(timestamp)
-    #print("Request status:")
-    #print(request_center_status)
-    #print("----------------------------------------------------------------------------------------------------------------------------------------------\n \n")
-    time.sleep(60*15)
 
-    
+# The occupancy data is requested every 15 minutes
 while True:
     request_all_centers()
+    time.sleep(60*15)
